2985fb3085280c397191d95b13ca4664a907f408
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-29 17:36:35 -0300

    First controller and Razor pages tests

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace WebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Aluno"] = "Charlles Lima";
 
             return View();
         }
