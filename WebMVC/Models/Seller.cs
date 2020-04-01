470a9b4a5965d1f9d76ddd3370c78c5c78c5b3ac
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-31 23:07:07 -0300

    Foreign key not null (referential integrity)

--- WebMVC/Models/Seller.cs
+++ WebMVC/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace WebMVC.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; } //para criar a FK no BD
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
